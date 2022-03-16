65879842ba77c4e4b38adcc501f18784ac45d4bc
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-03-15 22:55:32 -0300

    Adiciona imagem de fundo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
-
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
@@ -11,7 +9,21 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Image(
+                height: MediaQuery.of(context).size.height / 2.5,
+                width: double.infinity,
+                image: AssetImage("assets/images/login_background.jpg",),
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
