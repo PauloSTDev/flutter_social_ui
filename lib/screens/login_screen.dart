1be0fb9d2ae27d73abf6f029079a0b86d0b4ece2
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-03-16 21:29:47 -0300

    Adiciona Botão de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,42 +17,76 @@ class _LoginScreenState extends State<LoginScreen> {
               Image(
                 height: MediaQuery.of(context).size.height / 2.5,
                 width: double.infinity,
-                image: AssetImage("assets/images/login_background.jpg",),
+                image: AssetImage(
+                  "assets/images/login_background.jpg",
+                ),
                 fit: BoxFit.cover,
               ),
-              Text("FRENZY",
+              Text(
+                "FRENZY",
                 style: TextStyle(
                     color: Theme.of(context).primaryColor,
-                  fontSize: 34.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.0
-                ),
+                    fontSize: 34.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.0),
               ),
-              SizedBox(height: 10.0,),
+              SizedBox(
+                height: 10.0,
+              ),
               Padding(
-                padding: EdgeInsets.symmetric(vertical:  10.0, horizontal: 20.0),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                 child: TextField(
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.symmetric(vertical: 15.0),
                     fillColor: Colors.white,
                     filled: true,
                     hintText: "Username",
-                    prefixIcon: Icon(Icons.account_box, size: 30.0,),
+                    prefixIcon: Icon(
+                      Icons.account_box,
+                      size: 30.0,
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 10.0,),
+              SizedBox(
+                height: 10.0,
+              ),
               Padding(
-                padding: EdgeInsets.symmetric(vertical:  10.0, horizontal: 20.0),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                 child: TextField(
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.symmetric(vertical: 15.0),
                     fillColor: Colors.white,
                     filled: true,
                     hintText: "Password",
-                    prefixIcon: Icon(Icons.lock, size: 30.0,),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      size: 30.0,
+                    ),
                   ),
                   obscureText: true,
+                ),
+              ),
+              SizedBox(height: 40.0,),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 60.0),
+                  height: 45.0,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.5,
+                    ),
+                  ),
                 ),
               ),
             ],
