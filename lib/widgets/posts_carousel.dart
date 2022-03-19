048bcc38055793b5b4b721e3d34843ed2729705c
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-03-19 12:54:51 -0300

    Adiciona Stack de Containers para amostragem de Posts

--- lib/widgets/posts_carousel.dart
+++ lib/widgets/posts_carousel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_social_ui/models/post_model.dart';
 
@@ -8,8 +9,52 @@ class PostsCarousel extends StatelessWidget {
 
   PostsCarousel({this.pageController, this.posts, this.title});
 
+  _buildPost(BuildContext context, int index) {
+    Post post = posts[index];
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(15.0),
+            boxShadow: [BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            )]
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+          child: Text(
+            title,
+            style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0),
+          ),
+        ),
+        Container(
+          height: 400.0,
+          child: PageView.builder(
+            itemCount: posts.length,
+            controller: pageController,
+            itemBuilder: (BuildContext context, int index) {
+              return _buildPost(context, index);
+            },
+          ),
+        )
+      ],
+    );
   }
 }
