7e11af942393542eccdcc4148bf548b0bb5d6921
Paulo Santos <afonso.mea@gmail.com>
2022-03-24 09:01:15 -0300

    Adiciona imagem de fundo do perfil do usuário atual

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/data/data.dart';
 
 class CustomDrawer extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Image(
+                height: 200.0,
+                width: double.infinity,
+                image: AssetImage(currentUser.backgroundImageUrl),
+                fit: BoxFit.cover,
+              ),
+              Positioned(
+                  bottom: 20.0,
+                  left: 20.0,
+                  child: Row(
 
+                  ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
